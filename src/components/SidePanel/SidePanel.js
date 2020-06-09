f31977a71cd88b4de8dc403cab90df2c5e084ff6
Nozomu Tsuruta <61049074+NozomuTsuruta@users.noreply.github.com>
2020-06-09 19:17:51 +0900

    bug fix

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -3,6 +3,7 @@ import { Menu } from 'semantic-ui-react';
 import UserPanel from './UserPanel';
 import Channels from './Channels'
 import DirectMessages from './DirectMessage';
+import Starred from './Starred';
 
 const SidePanel = () => {
   return (
@@ -14,6 +15,7 @@ const SidePanel = () => {
       style={{ background: '#4c3c4c', fontSize: '1.2rem' }}
     >
       <UserPanel />
+      <Starred/>
       <Channels />
       <DirectMessages/>
     </Menu>
